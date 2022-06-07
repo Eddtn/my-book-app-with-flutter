56985e95f87ab625466d0932accaa7c718d6dd43
eddtn <eddietonsagie@gmail.com>
2022-06-07 02:35:18 +0100

    progress1

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -5,6 +5,262 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBar(
+            actions: [
+              Icon(
+                Icons.search,
+                size: 40,
+              ),
+            ],
+            title: Text('My Book App with Flutter'),
+            leading: Icon(Icons.menu),
+            backgroundColor: Colors.blue,
+            floating: true,
+            expandedHeight: 80,
+            pinned: true,
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 300,
+              width: 20,
+              color: Colors.grey,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                itemBuilder: (BuildContext context, index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: 350,
+                        width: 190,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.red,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        child: Container(
+                          height: 70,
+                          width: 190,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: const [
+                                  Text(
+                                    'book title',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  Text(
+                                    'book title',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                width: 90,
+                              ),
+                              const Text(
+                                'Free',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SliverToBoxAdapter(
+            child: SizedBox(
+              height: 5,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Icon(Icons.add_box),
+                      Text('choose what you want'),
+                      Icon(Icons.more_horiz)
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 40,
+              width: 100,
+              color: Colors.grey,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 7,
+                itemBuilder: (BuildContext context, index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 30,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 40,
+              width: 100,
+              color: Colors.grey,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 7,
+                itemBuilder: (BuildContext context, index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 30,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Row(
+                children: const [
+                  Icon(Icons.filter_1),
+                  SizedBox(width: 20),
+                  Text('FEATURED')
+                ],
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 300,
+              width: 20,
+              color: Colors.grey,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 4,
+                itemBuilder: (BuildContext context, index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 240,
+                        width: 160,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.red,
+                        ),
+                      ),
+                      Positioned(
+                        child: Container(
+                          height: 150,
+                          width: 190,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Container(
+                    height: 10,
+                    width: 25,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red,
+                    ),
+                  ),
+                  Container(
+                    height: 10,
+                    width: 25,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red,
+                    ),
+                  ),
+                  Container(
+                    height: 10,
+                    width: 25,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red,
+                    ),
+                  ),
+                  Container(
+                    height: 10,
+                    width: 25,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red,
+                    ),
+                  ),
+                  Container(
+                    height: 10,
+                    width: 25,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Row(
+                children: const [
+                  Icon(Icons.filter_1),
+                  SizedBox(width: 20),
+                  Text('MOST PLAYED')
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
